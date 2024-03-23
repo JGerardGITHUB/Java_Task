8a1608f47f45ea212f63e1254f0195dfd1736192
JGerardGITHUB <164190199+JGerardGITHUB@users.noreply.github.com>
2024-03-23 17:36:46 +0800

    Create tasksheet_1_1_1.java

---
+++ tasksheet_1_1_1.java
@@ -0,0 +1,10 @@
+/**https://www.onlinegdb.com/online_java_compiler#editor_1 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+ 
+class tasksheet_1_1_1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
