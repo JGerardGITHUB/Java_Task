c56588c3b5f94c16f33f0454a60f0e6b42683473
JGerardGITHUB <164190199+JGerardGITHUB@users.noreply.github.com>
2024-04-20 15:30:40 +0800

    Create tasksheet_1_1_11.java

---
+++ tasksheet_1_1_11.java
@@ -0,0 +1,33 @@
+class tasksheet_1_1_11 {
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+class Car extends Vehicle {
+    int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car("Mitsubishi", "Xpander", 2024, 5);
+        myCar.displayDetails();
+    }
+}
