9faa839c6ece9219322972dc470fb897a3e97b9b
JGerardGITHUB <164190199+JGerardGITHUB@users.noreply.github.com>
2024-04-26 18:23:14 +0800

    Update tasksheet_1_1_7.java

--- tasksheet_1_1_7.java
+++ tasksheet_1_1_7.java
@@ -1,29 +1,33 @@
-public class tasksheet_1_1_7 { 
-    
-	public static void main(String[] args) {
-    	int num1 = 9;
-    	int num2 = 8;
-	int addResult = addNumber(num1 , num2);
-	int subtractResult = subtractNumber(num1 , num2);
-	int multiplyResult = multiplyNumber(num1 , num2);
-	int divideResult = divideNumber(num1 , num2);
-	
-	System.out.println("num1 = " + num1 + "; num2 = " + num2 );
-	System.out.println("Sum: " +  addResult);
-	System.out.println("Difference: " +  subtractResult);
-	System.out.println("Product: " +  multiplyResult);
-	System.out.println("Quotient: " +  divideResult);
-	}
-		public static int addNumber (int num1, int num2) {
-	    return num1 + num2;
-	}
-	public static int subtractNumber (int num1, int num2) {
-	    return num1 - num2;
-	}
-	public static int multiplyNumber (int num1, int num2) {
-	    return num1 * num2;
-	}
-	public static int divideNumber (int num1, int num2) {
-	    return num1 / num2;
-	}
+import java.util.*;
+public class tasksheet_1_1_7 {
+    void addTwoNumbers(int a, int b){
+        System.out.println("the sum of "+a+"+"+b+"= "+ (a+b));
+    }
+    void minusTwoNumbers(int a, int b){
+        System.out.println("the difference of "+a+"-"+b+"= "+ (a-b));
+    }
+    void multiplyTwoNumbers(int a, int b){
+        System.out.println("the product of "+a+"*"+b+"= "+ (a*b));
+    }
+    void divideTwoNumbers(int a, int b){
+        System.out.println("the quotient of "+a+"/"+b+"= "+ (a/b));
+    }
+    void arithMethod(int a,int b){
+        addTwoNumbers(a, b);
+        minusTwoNumbers(a, b);
+        divideTwoNumbers(a, b);
+        multiplyTwoNumbers(a, b);
+    }
+ 
+    public static void main(String[] args) {
+        tasksheet_1_1_7 t7 = new tasksheet_1_1_7();
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter 2 numbers");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        t7.arithMethod(a, b);
+        input.close();
+        System.exit(0);
+    }
+
 }
