c1b8d014af24942cdef644e3c6a5c9f0fbf0998c
JGerardGITHUB <164190199+JGerardGITHUB@users.noreply.github.com>
2024-04-26 21:00:27 +0800

    Update tasksheet_1_1_12.java

--- tasksheet_1_1_12.java
+++ tasksheet_1_1_12.java
@@ -1,53 +1,63 @@
-interface tasksheet_1_1_12 {
+interface Shape {
     double calculateArea();
     double calculatePerimeter();
 }
 abstract class AbstractShape implements Shape {
-    String color;
-    double length;
-    double width;
+    protected String color;
+    protected double length;
+    protected double width;
 
-      public AbstractShape(String color, double length, double width) {
+    public AbstractShape(String color, double length, double width) {
         this.color = color;
         this.length = length;
         this.width = width;
     }
-        public abstract double calculateArea();
-        public abstract double calculatePerimeter();
+    @Override
+    public double calculateArea() {
+        return 0.0; 
+    }
+    @Override
+    public double calculatePerimeter() {
+        return 0.0; 
+    }
 }
-class Circle extends AbstractShape {
-    double radius;
+    class Circle extends AbstractShape {
+    private double radius;
 
-       public Circle(String color, double radius) {
+    public Circle(String color, double radius) {
         super(color, 0, 0); 
         this.radius = radius;
     }
+    @Override
     public double calculateArea() {
         return Math.PI * radius * radius;
     }
+    @Override
     public double calculatePerimeter() {
         return 2 * Math.PI * radius;
     }
 }
-class Rectangle extends AbstractShape {
-    
-        public Rectangle(String color, double length, double width) {
+    class Rectangle extends AbstractShape {
+    public Rectangle(String color, double length, double width) {
         super(color, length, width);
     }
+    @Override
     public double calculateArea() {
         return length * width;
     }
+    @Override
     public double calculatePerimeter() {
         return 2 * (length + width);
     }
 }
-public class Main {
+public class tasksheet_1_1_12 {
     public static void main(String[] args) {
-        Circle circle = new Circle("Blue", 4.0);
-        Rectangle rectangle = new Rectangle("Yellow", 3.0, 5.0);
+        Circle circle = new Circle("Red", 5.0);
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 3.0);
 
         System.out.println("Circle Area: " + circle.calculateArea());
         System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
+
         System.out.println("Rectangle Area: " + rectangle.calculateArea());
         System.out.println("Rectangle Perimeter: " + rectangle.calculatePerimeter());
     }
