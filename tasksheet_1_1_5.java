e446fc877749a32e3d83470f36c06c6280a1ece6
JGerardGITHUB <164190199+JGerardGITHUB@users.noreply.github.com>
2024-05-02 15:57:19 +0800

    Update tasksheet_1_1_5.java

--- tasksheet_1_1_5.java
+++ tasksheet_1_1_5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeChecker {
+public class tasksheet_1_1_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a word: ");
